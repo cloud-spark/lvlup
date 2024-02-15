d2cdef9085b90d582193da43df7cf964a7c086a5
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2024-02-14 16:43:38 -0800

    comments.

--- python/linked_list/double_link.py
+++ python/linked_list/double_link.py
@@ -20,13 +20,15 @@ class LinkedList:
   def removeHead(self):
     if self.head is None:
       return None
-    value = self.head.value
+    node = self.head.value
+    # set next and prev pointers of node to be removed to None, to help with garbage collection
+    node.next = node.prev = None
     self.head = self.head.next
     if self.head is not None:
       self.head.prev = None
     else:
       self.tail = None
-    return value
+    return node.value
   
   def insertTail(self, value):
     node = self.Node(value)
@@ -41,10 +43,12 @@ class LinkedList:
   def removeTail(self):
     if self.tail is None:
       return None
-    value = self.tail.value
+    node = self.tail.value
+    # set next and prev pointers of node to be removed to None, to help with garbage collection
+    node.next = node.prev = None
     self.tail = self.tail.prev
     if self.tail is not None:
       self.tail.next = None
     else:
       self.head = None
-    return value
+    return node.value
